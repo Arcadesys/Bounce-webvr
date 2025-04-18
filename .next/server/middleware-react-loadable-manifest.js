86f36dfb8d4dc041275c67363f750fe875ea4a59
Austen Tucker <austen@thearcades.me>
2025-04-17 20:25:51 -0500

    Update .gitignore and add Next.js build files, including package-lock.json and various server and static assets for the Bounce VR app.

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"pages/index.js -> @/components/BounceScene\":{\"id\":\"pages/index.js -> @/components/BounceScene\",\"files\":[\"static/chunks/components_BounceScene_js.js\"]}}"
